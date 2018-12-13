15a68f080171af872dd82177162314c42f1fe7ec
vimm0 <vimmrana0@gmail.com>
2018-12-13 18:02:56 +0545

    fix map

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,64 +1,68 @@
 <template>
-  <div class="contact-area section-padding-0-80">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <div class="contact-title">
-            <h2>Contact us</h2>
+  <div>
+    <div class="contact-area section-padding-0-80">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="contact-title">
+              <h2>Contact us</h2>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="row">
-        <div class="col-12 col-lg-8">
-          <div class="contact-form-area">
-            <form action="#" method="post">
-              <div class="row">
-                <div class="col-12 col-lg-6">
-                  <input type="text" class="form-control" id="name" placeholder="Name">
-                </div>
-                <div class="col-12 col-lg-6">
-                  <input type="email" class="form-control" id="email" placeholder="E-mail">
-                </div>
-                <div class="col-12">
-                  <input type="text" class="form-control" id="subject" placeholder="Subject">
-                </div>
-                <div class="col-12">
+        <div class="row">
+          <div class="col-12 col-lg-8">
+            <div class="contact-form-area">
+              <form action="#" method="post">
+                <div class="row">
+                  <div class="col-12 col-lg-6">
+                    <input type="text" class="form-control" id="name" placeholder="Name">
+                  </div>
+                  <div class="col-12 col-lg-6">
+                    <input type="email" class="form-control" id="email" placeholder="E-mail">
+                  </div>
+                  <div class="col-12">
+                    <input type="text" class="form-control" id="subject" placeholder="Subject">
+                  </div>
+                  <div class="col-12">
                   <textarea name="message" class="form-control" id="message" cols="30" rows="10"
                             placeholder="Message"></textarea>
+                  </div>
+                  <div class="col-12 text-center">
+                    <button class="btn newspaper-btn mt-30 w-100" type="submit">Send Message</button>
+                  </div>
                 </div>
-                <div class="col-12 text-center">
-                  <button class="btn newspaper-btn mt-30 w-100" type="submit">Send Message</button>
-                </div>
-              </div>
-            </form>
+              </form>
+            </div>
+          </div>
+
+          <div class="col-12 col-lg-4">
+            <!-- Single Contact Information -->
+            <div class="single-contact-information mb-30">
+              <h6>Address:</h6>
+              <p>481 Creekside Lane Avila <br>Beach, CA 93424</p>
+            </div>
+            <!-- Single Contact Information -->
+            <div class="single-contact-information mb-30">
+              <h6>Phone:</h6>
+              <p>+53 345 7953 32453 <br>+53 345 7557 822112</p>
+            </div>
+            <!-- Single Contact Information -->
+            <div class="single-contact-information mb-30">
+              <h6>Email:</h6>
+              <p>yourmail@gmail.com</p>
+            </div>
           </div>
         </div>
 
-        <div class="col-12 col-lg-4">
-          <!-- Single Contact Information -->
-          <div class="single-contact-information mb-30">
-            <h6>Address:</h6>
-            <p>481 Creekside Lane Avila <br>Beach, CA 93424</p>
-          </div>
-          <!-- Single Contact Information -->
-          <div class="single-contact-information mb-30">
-            <h6>Phone:</h6>
-            <p>+53 345 7953 32453 <br>+53 345 7557 822112</p>
-          </div>
-          <!-- Single Contact Information -->
-          <div class="single-contact-information mb-30">
-            <h6>Email:</h6>
-            <p>yourmail@gmail.com</p>
-          </div>
+        <!-- Google Maps -->
+        <div class="map-area">
+          <div id="googleMap"></div>
         </div>
-      </div>
 
-      <!-- Google Maps -->
-      <div class="map-area">
-        <div id="googleMap"></div>
       </div>
-
     </div>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAwuyLRa1uKNtbgx6xAJVmWy-zADgegA2s"></script>
+    <script src="js/google-map/map-active.js"></script>
   </div>
 </template>
